5e1e657632664d04529289fa9d4b66d5445f9548
Yonjay <dd257248@163.com>
2023-12-21 15:50:44 +0800

    fix: 修复info图标错误

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import { extname } from "path"
-import { Plugin as PluginImportToCDN } from "vite-plugin-cdn-import"
 import react from "@vitejs/plugin-react"
 import Compression from "vite-plugin-compression"
 import Inspect from "vite-plugin-inspect"
@@ -10,45 +9,43 @@ import AutoImport from "unplugin-auto-import/vite"
 const modelExts = [".gltf", ".glb", ".obj", "mtl", ".fbx", "stl", "vtp", "vtk", "ply", "xyz"]
 const cssExts = [".css", ".less", ".scss", "sass", ".stylus"]
 
-const cdnModules = [
-    {
-        name: "react",
-        var: "React",
-        path: `https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js`,
-    },
-    {
-        name: "konva",
-        var: "Konva",
-        path: "https://cdnjs.cloudflare.com/ajax/libs/konva/9.0.0/konva.min.js",
-    },
-    {
-        name: "antd",
-        var: "antd",
-        path: "https://cdnjs.cloudflare.com/ajax/libs/antd/5.10.2/antd.min.js",
-    },
-    {
-        name: "axios",
-        var: "axios",
-        path: "https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.1/axios.min.js",
-    },
-    {
-        name: "dayjs",
-        var: "dayjs",
-        path: "https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.11.10/dayjs.min.js",
-    },
-]
+const globals = {
+    react: "react",
+    konva: "konva",
+    antd: "antd",
+    axios: "axios",
+    xlsx: "xlsx",
+    jszip: "jszip",
+    "react-router": "react-router",
+    "react-dom": "react-dom",
+    "react-router-dom": "react-router-dom",
+    "styled-components": "styled-components",
+}
+
+function buildStart() {
+    function handleBuildStart(options) {
+        const { external, plugins } = options
+        console.log()
+
+    }
+
+    return {
+        name: "BuildStart",
+        buildStart: handleBuildStart,
+    }
+}
 
 export default defineConfig({
         plugins: [
             react(),
             Inspect({ build: true, outputDir: ".vite-inspect" }),
             Compression({ threshold: 1024 * 1024 * 2 }), // gzip : over 2Mb compression
-            PluginImportToCDN({ modules: cdnModules }),
             AutoImport({
                 imports: ["react", "react-router", "react-router-dom"],
                 dts: true,
                 include: [/\.[tj]sx?$/],
             }),
+            // buildStart(),
         ],
         server: {
             open: true,
@@ -58,20 +55,22 @@ export default defineConfig({
             alias: {
                 "@": fileURLToPath(new URL("./src", import.meta.url)),
             },
+            dedupe: Object.keys(globals),
         },
         build: {
             outDir: "dist",
             assetsDir: "assets",
             chunkSizeWarningLimit: 1500,
+
             rollupOptions: {
                 output: {
                     chunkFileNames: "assets/js/[name]-[hash].js",
                     entryFileNames: "assets/js/[name].[hash].js",
                     compact: true,
                     manualChunks: {
-                        react: ["react-router", "react-router-dom"],
+                        react: ["react", "react-router", "react-router-dom"],
                         redux: ["react-redux", "@reduxjs/toolkit"],
-                        "react-konva": ["konva", "react-konva", "react-konva-utils", "use-image"],
+                        konva: ["konva", "react-konva", "react-konva-utils", "use-image"],
                         "styled-components": ["styled-components"],
                     },
                     assetFileNames: chunkInfo => {
@@ -87,6 +86,7 @@ export default defineConfig({
 
                         return `assets/images/[name].[hash].[ext]`
                     },
+                    globals,
                 },
             },
             minify: true,
