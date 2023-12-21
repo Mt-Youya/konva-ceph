11d981d053e9c40320598feab2069619d599f1a5
youya <dd257248@163.com>
2023-12-21 19:00:28 +0800

    fix: 修复header背景

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -4,12 +4,19 @@ import InteractiveActions from "./components/InteractiveActions"
 import styled from "styled-components"
 
 const ScHeader = styled.header`
-    height: 110px;
+    height: 100px;
     color: #f9f9f9;
     user-select: none;
-    display: flex;
-    padding: 20px 6% 0 6%;
-    gap: 60px;
+    display: grid;
+    grid-template-columns: .6fr .8fr 1fr;
+    gap: 10%;
+    padding: 0 100px;
+    background-color: #1f2327;
+    backdrop-filter: saturate(180%) blur(20px);
+    box-shadow: 0 0 4px #292e33;
+    border-bottom-left-radius: 12px;
+    border-bottom-right-radius: 12px;
+    
     @media screen  and (max-width: 1980px) {
         gap: 20px;
         padding: 15px 3%;
